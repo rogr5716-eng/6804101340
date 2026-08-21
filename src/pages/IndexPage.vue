b042dcd8fba672af6ea77c042f63e5ad1d22bda1
ธนันธร ศิริ_6804101340 <rogr5716@gmail.com>
2026-08-21 15:09:58 +0700

    v.6-Button with Japanese language-340

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,12 @@
 <template>
   <q-page class="q-pa-md">
-    <q-form
-      class="q-gutter-md"
-      style="max-width: 600px"
-      @submit="onSubmit"
-      @reset="onReset"
-    >
-      <q-input
-        v-model="name"
-        filled
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[val => !!val || 'Name is required']"
-      />
-
-      <q-input
-        v-model="age"
-        filled
-        type="number"
-        label="Your age *"
-        lazy-rules
-        :rules="[val => val > 0 || 'Age must be greater than 0']"
-      />
-
-      <q-toggle
-        v-model="accept"
-        label="I accept the license and terms"
-      />
-
+    <q-form class="q-gutter-md" style="max-width: 600px">
+      <q-input filled label="Your name *" hint="Name and surname" />
+      <q-input filled type="number" label="Your age *" />
+      <q-toggle label="I accept the license and terms" />
       <div>
-        <q-btn label="SUBMIT" type="submit" color="primary" />
-        <q-btn
-          label="RESET"
-          type="reset"
-          color="primary"
-          flat
-          class="q-ml-sm"
-        />
+        <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
+        <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
   </q-page>
